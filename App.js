3bb30770fb1cdda9ece064c75ea1cb2ee4e8dfdd
shivamt0602 <106347133+shivamt0602@users.noreply.github.com>
2024-05-02 13:31:58 +0530

    using spacing and justify-content props in grid

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Paper } from '@material-ui/core' //{} inside these are components
+import { Typography, AppBar, Card, CardActions, CardContent,Button, CardMedia, CssBaseline, Grid, Toolbar, Container, Paper } from '@material-ui/core' //{} inside these are components
 // typography is just texts
 import { PhotoCamera } from '@material-ui/icons';
-
 function App() {
   return (
     <>
@@ -16,32 +15,20 @@ function App() {
       </AppBar>
       <main>
         <div>
-          <Container maxWidth="sm">
+          <Container maxWidth="sm" style={{marginTop:"100px"}}>
             <Typography variant='h2' align='center' color='textPrimary' gutterBottom>Photo Album</Typography>
             <Typography variant='h5' color='textSecondary' align='center'>hello this is shivam and this is a page where I'm uploading my coorg trip photos with my college friends in the year 2024.
             </Typography>
           </Container>
         </div>
         <div>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={3}>
-              <Paper>1</Paper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-            <Paper>2</Paper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-            <Paper>3</Paper>
-            </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-            <Paper>4</Paper>
-            </Grid>
-            {/* <Grid item>
-              <item>5</item>
+          <Grid container spacing={3} justifyContent='center' style={{ marginTop: '10px' }}>
+            <Grid item>
+            <Button variant="contained" color="primary">See my Photos</Button>
             </Grid>
             <Grid item>
-              <item>6</item>
-            </Grid> */}
+            <Button variant="outlined">Private Photos</Button>
+            </Grid>
           </Grid>
         </div>
       </main>
