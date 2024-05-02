363c480548fb8cdb0a39d99052cff39b7922c298
shivamt0602 <106347133+shivamt0602@users.noreply.github.com>
2024-05-02 13:02:25 +0530

    using grid component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core' //{} inside these are components
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Paper } from '@material-ui/core' //{} inside these are components
 // typography is just texts
 import { PhotoCamera } from '@material-ui/icons';
 
@@ -17,10 +17,32 @@ function App() {
       <main>
         <div>
           <Container maxWidth="sm">
-            <Typography variant='h5' align='center' color='textPrimary' gutterBottom>Photo Album</Typography>
-            <Typography variant='h5' color='textSecondary' align='center' paragraph>hello this is shivam and this is a page where I'm uploading my coorg trip photos with my college friends in the year 2024.
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>Photo Album</Typography>
+            <Typography variant='h5' color='textSecondary' align='center'>hello this is shivam and this is a page where I'm uploading my coorg trip photos with my college friends in the year 2024.
             </Typography>
           </Container>
+        </div>
+        <div>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>1</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+            <Paper>2</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+            <Paper>3</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+            <Paper>4</Paper>
+            </Grid>
+            {/* <Grid item>
+              <item>5</item>
+            </Grid>
+            <Grid item>
+              <item>6</item>
+            </Grid> */}
+          </Grid>
         </div>
       </main>
     </>
